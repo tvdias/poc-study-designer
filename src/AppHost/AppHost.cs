6f47be9d26573f3e80e224885104abf196e4787a
Thiago Dias <1681936+tvdias@users.noreply.github.com>
2026-02-07 18:08:14 +0000

    disable azure functions by default

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,32 +1,34 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Microsoft.Extensions.Configuration;
 
-var cache = builder.AddRedis("cache");
+var builder = DistributedApplication.CreateBuilder(args);
+var enableAzureFunctions = builder.Configuration.GetValue<bool>("EnableAzureFunctions", false);
+
 var postgres = builder.AddPostgres("postgres").AddDatabase("studydb");
-var serviceBus = builder.AddAzureServiceBus("servicebus")
-    .AddTopic("questions")
-    .AddTopic("projects");
 
 var api = builder.AddProject<Projects.Api>("api")
-    .WithReference(cache)
     .WithReference(postgres)
-    .WaitFor(cache)
     .WaitFor(postgres)
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
 var appDesigner = builder.AddViteApp("app-designer", "../Designer")
-    .WithReference(api)
-    .WaitFor(api);
+    .WithReference(api);
 
 var appAdmin = builder.AddViteApp("app-admin", "../Admin")
-    .WithReference(api)
-    .WaitFor(api);
+    .WithReference(api);
 
-builder.AddAzureFunctionsProject<Projects.CluedinProcessor>("func-cluedin-processor")
-    .WithReference(serviceBus);
+if (enableAzureFunctions)
+{
+    var serviceBus = builder.AddAzureServiceBus("servicebus")
+        .AddTopic("questions")
+        .AddTopic("projects");
 
-builder.AddAzureFunctionsProject<Projects.ProjectsProcessor>("func-projects-processor")
-    .WithReference(serviceBus);
+    builder.AddAzureFunctionsProject<Projects.CluedinProcessor>("func-cluedin-processor")
+        .WithReference(serviceBus);
+
+    builder.AddAzureFunctionsProject<Projects.ProjectsProcessor>("func-projects-processor")
+        .WithReference(serviceBus);
+}
 
 builder.Build().Run();
 
