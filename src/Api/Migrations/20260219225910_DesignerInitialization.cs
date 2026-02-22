5924df244fcbf77cfbd5f727553b0838f72d2152
Thiago Dias <1681936+tvdias@users.noreply.github.com>
2026-02-22 11:23:03 +0000

    Consolidate migrations

--- src/Api/Migrations/20260219225910_DesignerInitialization.cs
+++ src/Api/Migrations/20260219225910_DesignerInitialization.cs
@@ -177,6 +177,38 @@ namespace Api.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "FieldworkLanguages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FieldworkMarketId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LanguageCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    LanguageName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldworkLanguages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FieldworkLanguages_FieldworkMarkets_FieldworkMarketId",
+                        column: x => x.FieldworkMarketId,
+                        principalTable: "FieldworkMarkets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FieldworkLanguages_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "ManagedListItems",
                 columns: table => new
                 {
@@ -286,6 +318,8 @@ namespace Api.Migrations
                     AnswerMax = table.Column<int>(type: "integer", nullable: true),
                     QuestionFormatDetails = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
                     IsDummy = table.Column<bool>(type: "boolean", nullable: false),
+                    EditCustomAnswerCode = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    LockAnswerCode = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
                     CreatedBy = table.Column<string>(type: "text", nullable: true),
                     CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     ModifiedBy = table.Column<string>(type: "text", nullable: true),
@@ -447,6 +481,17 @@ namespace Api.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldworkLanguages_FieldworkMarketId",
+                table: "FieldworkLanguages",
+                column: "FieldworkMarketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldworkLanguages_StudyId_FieldworkMarketId_LanguageCode",
+                table: "FieldworkLanguages",
+                columns: new[] { "StudyId", "FieldworkMarketId", "LanguageCode" },
+                unique: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_ManagedListItems_ManagedListId_Value",
@@ -635,6 +680,9 @@ namespace Api.Migrations
 
             migrationBuilder.DropTable(
                 name: "StudyQuestionnaireLines");
+
+            migrationBuilder.DropTable(
+                name: "FieldworkLanguages");
 
             migrationBuilder.DropTable(
                 name: "ManagedListItems");
